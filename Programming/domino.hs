7081f4a34dc3e8ba29e9f0c041705790a4e96569
Anne-Greeth van Herwijnen <a.g.vanherwijnen@outlook.com>
2017-10-09 15:43:19 +0200

    Made bones en pips flexible

--- Programming/domino.hs
+++ Programming/domino.hs
@@ -11,17 +11,14 @@ type Pos    = (Int, Int)
 type Stone = (Pips,Pos)
 type Field = [Int]
 
-bones :: [Bone]
-bones = zip [1..] pips
+bones :: Int -> [Bone]
+bones max = zip [1..] (pips max)
 
-pips :: [Pips]
-pips = [(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),
-         (1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(2,2),
-         (2,3),(2,4),(2,5),(2,6),(3,3),(3,4),(3,5),
-         (3,6),(4,4),(4,5),(4,6),(5,5),(5,6),(6,6)]
+pips :: Int -> [Pips]
+pips n = [(x,y) | x <- [0..n], y <- [x..n]]
 
-findBoneValue :: Pips -> [Int]
-findBoneValue p = [ fst b | b <- bones, snd b == p ]
+findBoneValue :: Pips -> [Bone] -> [Int]
+findBoneValue p bns = [ fst b | b <- bns, snd b == p ]
 
 testGrid :: [[Int]]
 testGrid = [[6,6,2,6,5,2,4,1],
