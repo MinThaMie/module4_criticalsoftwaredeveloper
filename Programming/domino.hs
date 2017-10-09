d7ef76b9cdb2c18f87d27a8faaa211ac95bafc62
Anne-Greeth van Herwijnen <a.g.vanherwijnen@outlook.com>
2017-10-09 19:03:42 +0200

    Removed unused code to brainfarts

--- Programming/domino.hs
+++ Programming/domino.hs
@@ -97,23 +97,6 @@ removeAllUsedValues [] opts = opts
 removeAllUsedValues _ [] = []
 removeAllUsedValues (rmv:rmvs) opts = removeAllUsedValues rmvs (removeUsedValues rmv opts)
 
-removeFromBones :: Pips -> [Bone] -> [Bone]
-removeFromBones pip bns = filter ((/= pip).snd) bns
-
-removeStonesFromBones :: [Stone] -> [Bone] -> [Bone]
-removeStonesFromBones [] bns = bns
-removeStonesFromBones (((val1, val2), _) : stns) bns = removeStonesFromBones stns (removeFromBones (val1,val2) bns)
-
-solve :: Field -> Int -> IO()
-solve f max = putStr . unlines $ map show (chop 3 (solve' (bones max) (allOptions (grid f)) f))
-
-solve' :: [Bone] -> [Stone] -> Field -> Field 
-solve' b [] f = f
-solve' b (opt:opts) f | null b && null opts = f
-                      | null b || null opts = f
-                      | null (uniques opts) = solve' (removeStonesFromBones [opt] b) (removeAllUsedValues [opt] (removeAllUsedSquares [opt] opts)) (solution f [opt] b)
-                      | otherwise =  solve' (removeStonesFromBones (uniques (opt:opts)) b) (removeAllUsedSquares (uniques (opt:opts)) opts) (solution f (uniques (opt:opts)) b)
-
 chop :: Int -> [a] -> [[a]]
 chop n [] = []
 chop n xs = take n xs : chop n (drop n xs)
