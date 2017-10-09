a615375dd48c8850901927e8e494f8662a543b05
Anne-Greeth van Herwijnen <a.g.vanherwijnen@outlook.com>
2017-10-09 15:42:44 +0200

    Moved unused neighbours function

--- Programming/domino-brainfarts.hs
+++ Programming/domino-brainfarts.hs
@@ -29,3 +29,15 @@ uniques (x:xs) | x `elem` xs = uniques (filter (/= x) xs)
 
 
 
+
+neighbours :: Int -> Grid -> [Square]
+neighbours n g = concat (map maybeToList ([upneigh n g] ++ [downneigh n g] ++ [leftneigh n g] ++ [rightneigh n g]))
+
+upneigh :: Int -> Grid -> Maybe Square
+upneigh n g = if (n - 8) > 0              then Just (g !! (n - 8)) else Nothing
+
+downneigh n g = if (n + 8) < (length g)-1 then Just (g !! (n + 8)) else Nothing
+
+leftneigh n g = if n `mod` 8 /= 0         then Just (g !! (n - 1)) else Nothing
+
+rightneigh n g = if n `mod` 8 /= 7        then Just (g !! (n + 1)) else Nothing
--- Programming/domino.hs
+++ Programming/domino.hs
@@ -41,18 +41,6 @@ realGrid = grid field
 grid :: Field -> Grid
 grid g = zip g [0..] 
 
-neighbours :: Int -> Grid -> [Square]
-neighbours n g = concat (map maybeToList ([upneigh n g] ++ [downneigh n g] ++ [leftneigh n g] ++ [rightneigh n g]))
-
-upneigh :: Int -> Grid -> Maybe Square
-upneigh n g = if (n - 8) > 0              then Just (g !! (n - 8)) else Nothing
-
-downneigh n g = if (n + 8) < (length g)-1 then Just (g !! (n + 8)) else Nothing
-
-leftneigh n g = if n `mod` 8 /= 0         then Just (g !! (n - 1)) else Nothing
-
-rightneigh n g = if n `mod` 8 /= 7        then Just (g !! (n + 1)) else Nothing
-
 horizontalOptions :: Grid -> [Stone]
 horizontalOptions []        = []
 horizontalOptions [x]       = []
