3a733b3a4fe68720c7fbc1738031c0f322f8555c
Anne-Greeth van Herwijnen <a.g.vanherwijnen@outlook.com>
2017-10-11 14:06:30 +0200

    Implemented gameTree

--- Programming/Java/src/com/nedap/univeristy/domino/GameTree.java
+++ Programming/Java/src/com/nedap/univeristy/domino/GameTree.java
@@ -1,16 +1,47 @@
 package com.nedap.univeristy.domino;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by anne-greeth.vanherwijnen on 10/10/2017.
  */
 public class GameTree {
-	Board node;
-	GameTree tree;
+	private Board node;
+	private List<GameTree> children;
 
-	public GameTree(Board board, GameTree tree){
+	public GameTree(Board board) {
+		this.children = new ArrayList<>();
 		this.node = board;
-		this.tree = tree;
+		if (!board.isCorrect()) {
+			Bone chosen = board.getBones().get(0);
+			List<Stone> options = board.findStones(chosen.getPip1(), chosen.getPip2());
+			if (!options.isEmpty()) {
+				board.removeBone(0);
+				for (Stone s : options) {
+					Board newBoard = board.clone();
+					newBoard.getField().placeStone(s, chosen.getValue());
+					newBoard.removeFromOptions(s);
+					this.children.add(new GameTree(newBoard));
+				}
+			}
+		}
 	}
+
+	public List<Board.Field> findSolution(){
+		List<Board.Field> solutions = new ArrayList<>();
+		for (GameTree child: children) {
+			if (child.node.isFull()) {
+				solutions.add(child.node.getField());
+			} else {
+				solutions.addAll(child.findSolution());
+			}
+		}
+		return solutions;
+	}
+
+	private boolean isCorrect(){
+		return node.isCorrect();
+	}
+
 }
